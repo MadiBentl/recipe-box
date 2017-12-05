3e43c62672a3825b36e0146bf8683fe01717f23f
MadiBentley <mb.madisonbentley@gmail.com>
2017-12-04 21:40:39 -0800

    add recipes array

--- src/components/app.js
+++ src/components/app.js
@@ -1,3 +1,12 @@
 import React, { Component } from 'react';
 
+const recipes = [
+  {name: "Pumpkin Pie",
+   ingredients: ["Pumpkin Puree", "Sweetened Condensed Milk", "Eggs","Pumpkin Pie Spice", "Pie Crust"]},
+  {name: "Spaghetti",
+    ingredients: ["Noodles", "Tomato Sauce", "(Optional) Meatballs"]},
+  {name: "Onion Pie",
+    ingredients: ["Onion", "Pie Crust", "Sounds Yummy right?"]}
+];
+
 export default class App;
