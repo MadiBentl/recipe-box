90e2f43c3d697f4efa80d2af7a6922b97fa8442a
MadiBentley <mb.madisonbentley@gmail.com>
2017-12-04 22:09:22 -0800

    add Card

--- src/components/app.js
+++ src/components/app.js
@@ -1,15 +1,6 @@
 import React, { Component } from 'react';
 import Box from "./box";
 
-const recipes = [
-  {name: "Pumpkin Pie",
-   ingredients: ["Pumpkin Puree", "Sweetened Condensed Milk", "Eggs","Pumpkin Pie Spice", "Pie Crust"]},
-  {name: "Spaghetti",
-    ingredients: ["Noodles", "Tomato Sauce", "(Optional) Meatballs"]},
-  {name: "Onion Pie",
-    ingredients: ["Onion", "Pie Crust", "Sounds Yummy right?"]}
-];
-
 class App extends React.Component{
   constructor(props){
     super(props);
