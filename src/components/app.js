602270ade2eac0df2c54a790c6f5dbdb186e49fc
MadiBentley <mb.madisonbentley@gmail.com>
2017-12-04 21:31:49 -0800

    add initial files

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,3 @@
 import React, { Component } from 'react';
 
-export default class App extends Component {
-  render() {
-    return (
-      <div>React simple starter</div>
-    );
-  }
-}
+export default class App;
