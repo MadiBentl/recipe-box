5690d2adc69fd9b862256b64ea33530cb8d405bd
MadiBentley <mb.madisonbentley@gmail.com>
2017-12-05 22:57:54 -0800

    convert function to class

--- src/components/card.js
+++ src/components/card.js
@@ -1,16 +1,19 @@
 import React, {Component} from "react";
 import Ingredient from "./ingredient";
 
-const Card = ({recipe, ingredients}) => {
-  const recipeIngredients = ingredients.map((ingredient) => {
-    return (<Ingredient
-      ingredient = {ingredient}/>
-    )
-  });
-  return (<div>
-    <h4>{recipe.name}</h4>
-    <div>{recipeIngredients}</div>
-  </div>);
+class Card extends Component{
+//const Card = ({recipe, ingredients}) => {
+  render(){
+    const recipeIngredients = this.props.ingredients.map((ingredient) => {
+      return (<Ingredient
+        ingredient = {ingredient}/>
+      )
+    });
+    return (<div>
+      <h4>{this.props.recipe.name}</h4>
+      <div>{recipeIngredients}</div>
+    </div>);
+  };
 };
 
 export default Card;
