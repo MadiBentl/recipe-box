b45e9f1fc5b036afc864b68ceb7fe07ae4272759
MadiBentley <mb.madisonbentley@gmail.com>
2017-12-07 23:30:40 -0800

    add popup component

---
+++ src/components/pop-up.js
@@ -0,0 +1,6 @@
+import React, {Component} from "react";
+
+const Popup = () => {
+}
+
+export default Popup;
--- src/index.js
+++ src/index.js
@@ -1,26 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Popup from 'react-popup';
-
-ReactDOM.render(
-    <Popup />,
-    document.getElementById('popupContainer')
-);
-
-Popup.alert('Hello');
-/*import React from 'react';
-import ReactDOM from 'react-dom';
 import App from './components/app';
-import Popup from 'react-popup';
 
-Popup.alert('I am alert, nice to meet you');
 ReactDOM.render(
     <App />
   , document.querySelector('.container'));
-  ReactDOM.render(
-      <Popup />,
-      document.getElementById('popupContainer')
-  );
-
-  Popup.alert('Hello');
-*/
