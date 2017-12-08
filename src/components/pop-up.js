77b3e5c3b55522c9cd32a4bf61c469d187945b0d
MadiBentley <mb.madisonbentley@gmail.com>
2017-12-07 23:49:16 -0800

    bind this

--- src/components/pop-up.js
+++ src/components/pop-up.js
@@ -1,15 +1,33 @@
 import React, {Component} from "react";
 
-const Popup = () => {
-  return(
-    <div>
-    <h3>Add a recipe</h3>
-    <h4>Recipe</h4>
-    <input value="Recipe Name"></input>
-    <h4>Ingredients</h4>
-    <input value="Enter Ingredients,Separated,By Commas"></input>
-    </div>
-  )
+class Popup extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      recipeName: '',
+      recipeIngredients: ''
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+  render(){
+    return (
+      <div className="popup">
+      <h3>Add a recipe</h3>
+      <h4>Recipe</h4>
+      <input
+        value={this.state.recipeName}
+        onChange= {this.handleChange}/>
+      <h4>Ingredients</h4>
+      <input
+        value={this.state.recipeIngredients}
+        onChange= {this.handleChange}/>
+      </div>
+    );
+  }
+    handleChange(event) {
+      this.setState({recipeName: event.target.value});
+    }
 }
+
 
 export default Popup;
